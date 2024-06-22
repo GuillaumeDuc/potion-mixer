87e98682585a0867c0ca86e3c7c0b97e7ceb125e
GuillaumeDuc <ducoeurguillaume@gmail.com>
2024-06-22 09:16:55 +0200

    smoke shader

--- Assets/Smoke/2DBlur.hlsl
+++ Assets/Smoke/2DBlur.hlsl
@@ -10,8 +10,6 @@ float4 blur(UnityTexture2D MainTex, float4 UV, float Strength)
     for (int x = 0; x < Strength; x++) {
         output += tex2D(MainTex, UV + float2(-x, 0) * MainTex.texelSize.xy);
         output2 += tex2D(MainTex, UV + float2(x, 0) * MainTex.texelSize.xy);
-        output3 += tex2D(MainTex, UV + float2(0, x) * MainTex.texelSize.xy);
-        output4 += tex2D(MainTex, UV + float2(0, -x) * MainTex.texelSize.xy);
         sum++;
     }
 
